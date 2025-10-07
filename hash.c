6f43390674a812653be3d230518feeb1426bfd73
Colin S. <crszbt@umsystem.edu>
2025-10-06 21:16:11 -0500

    Attempted to write the functions associated with the no collision table

--- hash.c
+++ hash.c
@@ -9,12 +9,25 @@
 int InsertFailCollision(	void * hashtable, int elementSize, int elementCountMax,
                             int key, void * element, int (*HashFunc)(int key))
 {
-    return 0;
+	int hash = HashFunc(key); //gets the hash based on the inputted key
+	if(*(hashtable+(elementSize*hash))!=NULL || hash>elementCountMax) //if that hash is unavailable...
+	{
+		return -1; //returns -1
+	}
+	else
+	{
+		hashtable+(elementSize*hash) = element; //places the element in the proper address location in the hash table
+	}
+    return 0; //returns 0 if no errors are present
 }
 
 void * SearchNoCollision(void * hashtable, int key, int elementSize, int (*HashFunc)(int key))
 {
-	
+	int hash = HashFunc(key); //gets the hash based on the inputted key
+	if(*(hashtable+(elementSize*hash))!=NULL) //if there is an element at that hash...
+	{
+		return *(hashtable+(elementSize*hash)); //returns the element at the propor hash
+	}
     return NULL;
 }
 
