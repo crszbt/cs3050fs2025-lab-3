20cf19ff2a597f5ce72939454f75d2e481475ec8
Colin S. <crszbt@umsystem.edu>
2025-10-06 21:37:00 -0500

    Attempted to implement MOST of the chain hash table

--- hash.c
+++ hash.c
@@ -45,10 +45,18 @@ int MultMethod(int key)
     return hash;
 }
 
+typedef struct //creates struct for linked list
+{
+void* data;
+int key;
+void* next = NULL;
+}Link;
 
 void * AllocateChainTable(int elementCountMax)
 {
-    return NULL;
+	void* hashtable; //creates hash table
+	hashtable = malloc(elementCountMax*sizeof(void*)); //allocates space for hash table
+    return hashtable;
 }
 
 void FreeChainTable(void * hashtable)
@@ -59,11 +67,46 @@ void FreeChainTable(void * hashtable)
 int InsertChain(	void * hashtable, int elementSize, int elementCountMax,
                             int key, void * element, int (*HashFunc)(int key))
 {
-    return 0;
+	int hash = HashFunc(key); //gets the hash based on the inputted key
+	if(hash>elementCountMax) //if that hash is nonexistent...
+	{
+		return -1; //returns -1
+	}
+	if(*(hashtable+(elementSize*hash))!=NULL) //if that hash is occupied...
+	{
+		void * i = (*(hashtable+(elementSize*hash))); //creates iterator to travel down chain
+		while (i->next != NULL)
+			{
+				i = i->next;
+			}
+		Link * newlink = malloc(sizeof(Link)); //allocates space for new chain link
+		newlink->data = element; //stores element data in new chain link
+		newlink->next = NULL;
+		newlink->key = key; //gives new chain correct key
+		i->next = newlink; //adds new link to the chain
+	}
+	else
+	{
+		Link * newlink = malloc(sizeof(Link)); //allocates space for new chain link
+		newlink->data = element; //stores element data in new chain link
+		newlink->next = NULL;
+		newlink->key = key; //gives new chain correct key
+		*(hashtable+(elementSize*hash)) = newlink; //adds first link to the chain
+	}
+    return 0; //returns 0 if no errors are present
 }
 
 void * SearchChain(void * hashtable, int key, int elementSize, int (*HashFunc)(int key))
 {
-	
+	int hash = HashFunc(key); //gets the hash based on the inputted key
+	if(*(hashtable+(elementSize*hash))!=NULL) //if there is an element at that hash...
+	{
+		void * i = (*(hashtable+(elementSize*hash))); //creates iterator to travel chain
+		while(i->key != key) //iterates down chain until the propor key is found
+			{
+				i = i->next;
+			}
+		return i->data; //returns the element at the propor hash
+	}
     return NULL;
 }
